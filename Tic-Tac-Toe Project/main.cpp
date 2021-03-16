74ffcd0fcf4ea5739eb1ec2ae2b94f3aa7381127
caleb <caleb@DESKTOP-87OVU1K>
2021-03-16 16:03:57 +0000

    adds checkWinner method

--- Tic-Tac-Toe Project/main.cpp
+++ Tic-Tac-Toe Project/main.cpp
@@ -126,13 +126,23 @@ string checkWinner(string gameBoard[ROWS][COLS])
 
 	for (int i = 0; i < COLS; i++)
 	{
-		if (gameBoard[0][i] != " " && gameBoard[0][i] == gameBoard[0][i] && gameBoard[0][] == gameBoard[0][i])
+		if (gameBoard[0][i] != " " && gameBoard[0][i] == gameBoard[1][i] && gameBoard[1][i] == gameBoard[2][i])
 		{
 			return gameBoard[0][i];
 		}
 	}
+	if (gameBoard[0][0] != " " && gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2])
+	{
+		return gameBoard[0][0];
+	}
+	if (gameBoard[2][0] != " " && gameBoard[2][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[0][2])
+	{
+		return gameBoard[2][0];
+	}
 
+	return "";
 };
+
 
 //bool IsBoardFull(string gameBoard[ROWS][COLS])
 //{}
